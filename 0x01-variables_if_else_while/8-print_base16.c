95d4efac518b9a7224bc016c41e9346c076436e9
KeeneKabalisa <keenekabalisa@gmail.com>
2022-03-11 18:39:13 -0800

    Base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Base16
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 48;
+
+	while (i < 58)
+	{
+		putchar(i);
+		i++;
+	}
+
+	i = 97;
+	
+	while (i < 103)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar(10);
+	return (0);
+}
