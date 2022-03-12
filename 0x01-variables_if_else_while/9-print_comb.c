939c9b4d3bad0334835c95272f20a835b6c95a04
KeeneKabalisa <keenekabalisa@gmail.com>
2022-03-11 18:50:21 -0800

    Print comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <std.io>
+
+/**
+ * main - all nums
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 48;
+
+	while (i < 58)
+	{
+		putchar(i);
+		if (i < 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
+		i++;
+	}
+	putchar(10);
+	return (0);
+}
