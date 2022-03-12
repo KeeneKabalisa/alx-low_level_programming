5a0408e2ea3f6aaf12ebe0572815afa6cd627c32
KeeneKabalisa <keenekabalisa@gmail.com>
2022-03-11 18:28:10 -0800

    Print Num

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Print numbers with putchar
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 48;
+
+	while (i < 58)
+	{
+		putchar(i);
+		i++;
+	}
+	return (0);
+}
