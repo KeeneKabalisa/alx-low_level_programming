32cf03181371fa9f83a5d24b8aab39ff83f7fd14
KeeneKabalisa <keenekabalisa@gmail.com>
2022-03-11 17:58:20 -0800

    Alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Both case alpha
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	while (i < 26)
+	{
+		putchar(i + 97);
+		i++;
+	}
+
+	i = 0;
+
+	while (i < 26)
+	{
+		putchar(i + 65);
+		i++
+	}
+	printf("\n");
+	return (0);
+}
