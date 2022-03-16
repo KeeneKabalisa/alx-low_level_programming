6de4da3f34ace1ef6ebe8ec00a5af3453e526cb4
KeeneKabalisa <keenekabalisa@gmail.com>
2022-03-16 11:59:36 -0700

    Everytime

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * jack_bauer - print every minute
+ *
+ * Return: nothing
+ */
+void jack_bauer(void)
+{
+	int hour = 0;
+	int hour2 = 0;
+	int min = 0;
+	int min2 = 0;
+
+	while (hour < 3)
+	{
+		while (hour2 < 4)
+		{
+			while (min < 7)
+			{
+				while (min2 < 10)
+				{
+					_putchar(hour);
+					_putchar(hour2);
+					_putchar(':');
+					_putchar(min);
+					_putchar(min2);
+					_putchar('\n');
+					min2++;
+				}
+				min++;
+			}
+			hour2++;
+		}
+		hour++;
+	}
+}
