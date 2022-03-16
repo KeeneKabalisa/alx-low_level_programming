052b96a01171f5dc711a7753b4b3c3471fe86218
KeeneKabalisa <keenekabalisa@gmail.com>
2022-03-15 18:07:18 -0700

    10 Alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_alphabet_x10 - ten alphabet
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	char ch = 'a';
+	int i = 0;
+
+	while (i < 10)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			putchar(ch);
+			putchar(10);
+		}
+	}
+}
