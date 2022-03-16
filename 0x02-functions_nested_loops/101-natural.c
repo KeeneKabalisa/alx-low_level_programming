d1ffb442beaac8f148fd04555569752fe59fccb4
KeeneKabalisa <keenekabalisa@gmail.com>
2022-03-16 15:57:59 -0700

    Multiples

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - multiples of 3 & 5
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int max = 1024;
+	int sum = 0;
+	
+	for (int i = 0; i < max; i++)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+			sum = sum + i;
+	}
+	printf("%d", sum);
+	return (0);
+}
