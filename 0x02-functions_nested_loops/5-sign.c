bb4843e4116866d51eea3484f8dd017ab6c38b69
KeeneKabalisa <keenekabalisa@gmail.com>
2022-03-15 19:29:36 -0700

    Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_sign: print sign of number
+ * @n: input
+ *
+ * Return: 1 if n is > 0, 0 if n is zero and -1 if n < 0
+ */
+
+int print_sign(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		i = 1;
+		putchar('+');
+	}
+	else if (n == 0)
+	{
+		i = 0;
+		putchar('0');
+	}
+	else
+	{
+		i = -1;
+		putchar('-');
+	}
+	return (i);
+}
