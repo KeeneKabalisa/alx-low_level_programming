d06cf2fc9147a7f3591f0f196569b2ce8460a804
KeeneKabalisa <keenekabalisa@gmail.com>
2022-03-16 12:03:11 -0700

    Everytime

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,18 +7,18 @@
  */
 void jack_bauer(void)
 {
-	int hour = 0;
-	int hour2 = 0;
-	int min = 0;
-	int min2 = 0;
+	int hour = '0';
+	int hour2 = '0';
+	int min = '0';
+	int min2 = '0';
 
-	while (hour < 3)
+	while (hour < '3')
 	{
-		while (hour2 < 4)
+		while (hour2 < '4')
 		{
-			while (min < 7)
+			while (min < '7')
 			{
-				while (min2 < 10)
+				while (min2 < '10')
 				{
 					_putchar(hour);
 					_putchar(hour2);
