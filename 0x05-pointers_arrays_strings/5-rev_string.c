69996f13fc01208900a2b04f880a6f9a7736c5b3
KeeneKabalisa <keenekabalisa@gmail.com>
2022-03-22 00:21:57 +0200

    RevStr

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * rev_string - Reverses a string
+ *
+ * @s: input string
+ * Return: nothing
+ */
+void rev_string(char *s)
+{
+	int i = 0;
+	int j = 0;
+
+	while (s[i] != 0)
+		i++;
+	char c[i];
+
+	while ((i - 1) >= 0)
+	{
+		c[j] = s[i - 1];
+		j++;
+		i--;
+	}
+	strcpy(s, c);
