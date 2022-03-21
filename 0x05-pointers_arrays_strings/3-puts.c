c401e20acb3a212fd4f3554b2e6f3d1539d781e9
KeeneKabalisa <keenekabalisa@gmail.com>
2022-03-21 22:38:16 +0200

    PritntString

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts - Prints a string
+ *
+ * @str: Stirng input
+ *
+ * Return: nothing
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != 0)
+	{
+		_putchar(str[i]);
+		i++
+	}
+	_putchar('\n');
+}
