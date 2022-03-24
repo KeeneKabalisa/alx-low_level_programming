35897c4547ce555fcab3dcc75549d0ab0ceb6801
KeeneKabalisa <keenekabalisa@gmail.com>
2022-03-25 00:49:44 +0200

    Rev Array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverse array
+ * @a: array
+ * @n: int input 
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i;
+	int b;
+
+	i = 0;
+	while (i < n)
+	{
+		n--;
+		b = a[i];
+		a[i] = a[n];
+		a[n] = b;
+		i++;
+	}
+}
