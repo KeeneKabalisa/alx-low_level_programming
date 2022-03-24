17b6a31cc928251ccb6a5351508c4444afa3d612
KeeneKabalisa <keenekabalisa@gmail.com>
2022-03-24 23:48:46 +0200

    String concat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strcat - string concatenation
+ * @src: string input
+ * @dest: string input and destination
+ * Return: dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
