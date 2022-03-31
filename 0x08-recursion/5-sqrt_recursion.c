f7625122873dd7e9bf424153a123f1ae19be9c32
KeeneKabalisa <keenekabalisa@gmail.com>
2022-03-31 04:37:18 +0200

    Update

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -9,15 +9,15 @@
  */
 int _sqrt_recur(int n, int x)
 {
-        if (x * x == n)
-        {
-                return (x);
-        }
-        else if (x * x > n)
-        {
-                return (-1);
-        }
-        return (_sqrt_recur(n, x + 1));
+	if (x * x == n)
+	{
+		return (x);
+	}
+	else if (x * x > n)
+	{
+		return (-1);
+	}
+	return (_sqrt_recur(n, x + 1));
 }
 
 /**
