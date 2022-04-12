db2ef79a2468f9262b2ed5d6849b8647959f6510
KeeneKabalisa <keenekabalisa@gmail.com>
2022-04-12 04:23:45 +0200

    dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,14 @@
 #ifndef DOG_H
 #define DOG_H
 
+/*
+ * struct dog - user defined dog structure
+ *
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog owner's name
+ */
+
 struct dog 
 {
 	char *name;
